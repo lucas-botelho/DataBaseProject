22ba7941aeb2fc11ada3e55cf2b2aca69875e757
DESKTOP-2MTF3A5\lbotelho <l_botelho@hotmail.com>
2023-12-29 21:15:06 +0000

    etapa 1

--- Etapa_1.sql
+++ Etapa_1.sql
@@ -217,21 +217,79 @@ IGNORE 1 lines
 
 
 -- estes loads falham
+
+CREATE TABLE InscricaoDump (
+    dados varchar(2000)
+);
+
 LOAD DATA INFILE 'E:/Lusofona/Semestre 3/Bases de Dados/Projeto Final/[EI-IG-23.BD]dados/Inscricao.csv'
-INTO TABLE Inscricao
-FIELDS TERMINATED BY ';' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
+INTO TABLE InscricaoDump
 IGNORE 1 lines
+
+select count(*) from inscricaoDump -- 690 linhas
+select * from inscricaoDump
+
+INSERT INTO Inscricao (id, estado, idNumeroGrupo, idTFC, numeroAluno, ordemEscolha, registoDeInscricao, publicado, anoLetivo)
+SELECT
+    SUBSTRING_INDEX(SUBSTRING_INDEX(dados, ';', 1), ';', -1) AS id,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(dados, ';', 2), ';', -1) AS estado,
+    NULLIF(SUBSTRING_INDEX(SUBSTRING_INDEX(dados, ';', 3), ';', -1), '') AS idNumeroGrupo,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(dados, ';', 4), ';', -1) AS idTFC,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(dados, ';', 5), ';', -1) AS numeroAluno,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(dados, ';', 6), ';', -1) AS ordemEscolha,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(dados, ';', 7), ';', -1) AS registoDeInscricao,
+    NULLIF(SUBSTRING_INDEX(SUBSTRING_INDEX(dados, ';', 8), ';', -1), '') AS publicado,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(dados, ';', 9), ';', -1) AS anoLetivo
+FROM InscricaoDump;
+
+drop table InscricaoDump
+
+CREATE TABLE tfc.tfcdump (
+    id VARCHAR(1024) NULL,
+    Titulo VARCHAR(1024) NULL,
+    anoLetivo VARCHAR(1024) NULL,
+    avaliacaoProposta VARCHAR(1024) NULL,
+    coorientador VARCHAR(1024) NULL,
+    dataEstado VARCHAR(1024) NULL,
+    dataProposta VARCHAR(1024) NULL,
+    descricao VARCHAR(1024) NULL,
+    entidade VARCHAR(1024) NULL,
+    estado VARCHAR(1024) NULL,
+    idGrupo VARCHAR(1024) NULL,
+    idtfc VARCHAR(1024) NULL,
+    motivoRecusa VARCHAR(1024) NULL,
+    numeroInscricoes VARCHAR(1024) NULL,
+    orientador VARCHAR(1024) NULL,
+    orientadorProposto VARCHAR(1024) NULL,
+    preponente VARCHAR(1024) NULL,
+    semestre VARCHAR(1024) NULL,
+    tecnologias VARCHAR(1024) NULL
+);
+
 
 LOAD DATA INFILE 'E:/Lusofona/Semestre 3/Bases de Dados/Projeto Final/[EI-IG-23.BD]dados/TFC.csv'
-INTO TABLE TFC
+INTO TABLE tfcdump
 FIELDS TERMINATED BY ';' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
-IGNORE 1 lines
+IGNORE 1 LINES
+(id, Titulo, anoLetivo, avaliacaoProposta, coorientador, dataEstado, dataProposta, descricao, entidade, estado, idGrupo, idtfc, motivoRecusa, numeroInscricoes, orientador, orientadorProposto, preponente, semestre, tecnologias);
 
 
+drop table TFCDump
+
+CREATE TABLE tfc.tfcdump2 (
+    texto varchar(4000) NULL
+);
+
+LOAD DATA INFILE 'E:/Lusofona/Semestre 3/Bases de Dados/Projeto Final/[EI-IG-23.BD]dados/TFC.csv'
+INTO TABLE tfcdump2
+FIELDS TERMINATED BY ';' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(@id, @Titulo, @anoLetivo, @avaliacaoProposta, @coorientador, @dataEstado, @dataProposta, @descricao, @entidade, @estado, @idGrupo, @idtfc, @motivoRecusa, @numeroInscricoes, @orientador, @orientadorProposto, @preponente, @semestre, @tecnologias)
+SET texto = CONCAT_WS('|', @id, @Titulo, @anoLetivo, @avaliacaoProposta, @coorientador, @dataEstado, @dataProposta, @descricao, @entidade, @estado, @idGrupo, @idtfc, @motivoRecusa, @numeroInscricoes, @orientador, @orientadorProposto, @preponente, @semestre, @tecnologias);
 
 
 
