f5c672cea57f8cd910124ae28f314306014bfda0
DESKTOP-2MTF3A5\lbotelho <l_botelho@hotmail.com>
2023-12-31 15:59:42 +0000

    etapa 2

--- etapa2final.sql
+++ etapa2final.sql
@@ -8,34 +8,35 @@
 
 use tfc
 
--- Curso
-ALTER TABLE Curso ADD CONSTRAINT PK_Curso_id PRIMARY KEY (id);
-
--- Tecnologia
-ALTER TABLE Tecnologia ADD CONSTRAINT PK_Tecnologia_id PRIMARY KEY (id);
-
--- Disciplina
-ALTER TABLE Disciplina ADD CONSTRAINT PK_Disciplina_id PRIMARY KEY (id);
-
 -- Aluno
 ALTER TABLE Aluno ADD CONSTRAINT PK_Aluno_numeroAluno PRIMARY KEY (numeroAluno);
-
--- ProfessorDEISI
-ALTER TABLE ProfessorDEISI ADD CONSTRAINT PK_ProfessorDEISI_numeroProfessor PRIMARY KEY (numeroProfessor);
-
+--AvaliacaoDisciplinaAluno
+ALTER TABLE AvaliacaoDisciplinaAluno ADD CONSTRAINT PK_AvaliacaoDisciplinaAluno_id PRIMARY KEY (id);
+-- Curso
+ALTER TABLE Curso ADD CONSTRAINT PK_Curso_id PRIMARY KEY (id);
+-- Disciplina
+ALTER TABLE Disciplina ADD CONSTRAINT PK_Disciplina_id PRIMARY KEY (id);
+-- Empresa_EntidadeExterna 
+ALTER TABLE Empresa_EntidadeExterna ADD CONSTRAINT PK_Empresa_EntidadeExterna_idEmpresa PRIMARY KEY (id);
 -- Grupo 
 ALTER TABLE Grupo ADD CONSTRAINT PK_Grupo_id PRIMARY KEY (id);
-
--- TFC 
-ALTER TABLE TFC ADD CONSTRAINT PK_TFC_id PRIMARY KEY (id);
-
+--HistoricoTFC
+ALTER TABLE HistoricoTFC ADD CONSTRAINT PK_HistoricoTFC_id PRIMARY KEY (id);
+--Inscricao
+ALTER TABLE Inscricao ADD CONSTRAINT PK_Inscricao_id PRIMARY KEY (id);
+-- ProfessorDEISI
+ALTER TABLE ProfessorDEISI ADD CONSTRAINT PK_ProfessorDEISI_numeroProfessor PRIMARY KEY (numeroProfessor);
 -- ProfessorNDEISI
 ALTER TABLE ProfessorNDEISI ALTER COLUMN idProfessor varchar(255) NOT NULL;
 ALTER TABLE ProfessorNDEISI ADD CONSTRAINT PK_ProfessorNDEISI_idProfessor PRIMARY KEY (idProfessor);
-
--- Empresa_EntidadeExterna 
-ALTER TABLE Empresa_EntidadeExterna ADD CONSTRAINT PK_Empresa_EntidadeExterna_idEmpresa PRIMARY KEY (id);
-
+-- Tecnologia
+ALTER TABLE Tecnologia ADD CONSTRAINT PK_Tecnologia_id PRIMARY KEY (id);
+-- TFC 
+ALTER TABLE TFC ADD CONSTRAINT PK_TFC_id PRIMARY KEY (id);
+-- TFCCurso 
+ALTER TABLE TFCCurso ADD CONSTRAINT PK_TFCCurso_id PRIMARY KEY (id);
+-- TFCTecnologia 
+ALTER TABLE TFCTecnologia ADD CONSTRAINT PK_TFCTecnologia_id PRIMARY KEY (id);
 -- Utilizador
 ALTER TABLE Utilizador ALTER COLUMN idIdentificacao varchar(255) NOT NULL;
 ALTER TABLE Utilizador ADD CONSTRAINT PK_Utilizador_idIdentificacao PRIMARY KEY (idIdentificacao);
